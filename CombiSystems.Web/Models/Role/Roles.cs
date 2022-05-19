2cae9e742f0a35846115007a82e7b78a675cbd6d
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-05-19 16:26:15 +0300

    Template OK

---
+++ CombiSystems.Web/Models/Role/Roles.cs
@@ -0,0 +1,17 @@
+﻿namespace CombiSystems.Web.Models.Role
+{
+    public class Roles
+    {
+        public static readonly string Admin = "Admin";
+        public static readonly string Technician = "Technician";
+        public static readonly string Operator = "Operator";
+        public static readonly string User = "User";
+
+        public static List<string> RoleList = new List<string>()
+        {
+            Admin,Technician,Operator,User
+        };
+
+    }
+}
+
