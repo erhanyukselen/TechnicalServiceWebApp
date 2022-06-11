14679a4e9b0e4074175c1a4b44b5c015e402b9d1
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-06-11 21:32:37 +0300

    Appointmen Adress Add

--- CombiSystems.Core/Entities/Appointment.cs
+++ CombiSystems.Core/Entities/Appointment.cs
@@ -1,4 +1,4 @@
-using CombiSystems.Core.Entities.Abstracts;
+﻿using CombiSystems.Core.Entities.Abstracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +17,8 @@ public class Appointment : BaseEntity<int>
 
     public string? Description { get; set; }
 
+    public string? AppointmentAddress { get; set; }
+
     public bool TaskStatus { get; set; }=false;
 
     public string? TechnicianId { get; set; }
@@ -25,3 +27,4 @@ public class Appointment : BaseEntity<int>
 
 
 }
+
